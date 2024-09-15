cf13cf4e228c757772aca175000c012f3828efd3
“lilizhi <@lilizhi>
2024-09-16 00:57:57 +0800

    基础搭建

--- llz_springboot-service/src/main/java/com/llz/controller/BookController.java
+++ llz_springboot-service/src/main/java/com/llz/controller/BookController.java
@@ -1,4 +1,29 @@
 package com.llz.controller;
 
+
+import com.llz.common.enums.ResultCodeEnum;
+import com.llz.controller.vo.ResultVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 测试控制类
+ */
+@Controller
+@Slf4j
+@RequestMapping("/api/llz")
 public class BookController {
+    /**
+     * 测试接口
+     * @return
+     */
+    @GetMapping("/test01")
+    public ResultVO<Object> test01(){
+      log.info("接口请求:{}",1);
+      return ResultVO.success("请求成功");
+    }
+
+
 }
---
+++ llz_springboot-service/src/main/java/com/llz/controller/vo/ResultVO.java
@@ -0,0 +1,33 @@
+package com.llz.controller.vo;
+
+
+import com.llz.common.enums.ResultCodeEnum;
+import lombok.Data;
+
+/**
+ * 返回VO
+ */
+@Data
+public class ResultVO<T> {
+
+    private String code;
+
+    private String msg;
+
+    private T data;
+
+    public static <T>ResultVO<T> success(String msg){
+        ResultVO<T> resultVO = new ResultVO<T>();
+        resultVO.code = ResultCodeEnum.SUCCESS.getCode();
+        resultVO.msg = msg;
+        return resultVO;
+    }
+
+    public static <T>ResultVO<T> success(T data){
+        ResultVO<T> resultVO = new ResultVO<T>();
+        resultVO.code = ResultCodeEnum.SUCCESS.getCode();
+        resultVO.msg = "成功";
+        resultVO.data = data;
+        return resultVO;
+    }
+}
